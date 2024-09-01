831252674753b720b726a95b5bea15ee38863bf2
wanglikang <1024196018@q.com>
2024-09-01 22:31:31 +0800

    直接集成.drawio文件，不用再导出为html了

--- README.md
+++ README.md
@@ -6,20 +6,12 @@ A Hexo plugin, used to embed drawio files everywhere locally
 
 
 # Usage
-## 1、edit and export
-
-Use desktop or online editing
-
-After editing, export to HTML format
-
-![img.png](./imgs/export_01.png)
-
 
 ## Integrated into Hexo
 
 use hexo's tag **drawio_file_path** 
 ```
-{% drawio_file_path ../drawioSources/Redis.drawio.html %}
+{% drawio_file_path ../drawioSources/Redis.drawio %}
 ```
 
 ![img.png](./imgs/usage_01.png)
@@ -34,3 +26,21 @@ after generate static files. it looks like
 
 see detail : https://github.com/wanglikang/hexo-drawio
 
+# 备忘
+本地链接本插件
+```shell
+npm link
+```
+
+在本地的其他项目中引用
+```shell
+npm link hexo-drawio
+```
+
+## 发布
+
+发布的时候，可以去npm上申请一个publish的token，然后配置到项目的.npmrc里
+```shell
+//registry.npmjs.org/:_authToken=${NPM_TOKEN}
+```
+这样，就不用每次发布都需要输入用户名和密码了
--- index.js
+++ index.js
@@ -15,21 +15,29 @@ async function parseBodyFromHtml(sourceContent) {
 async function generateTagFromSource(context, sourcePath,  options) {
     const start = performance.now();
     const drawioFilePathInPost = options.file;
-    const drawioFullPath = path.dirname(sourcePath);
-    // console.log(drawioFilePathInPost)
-    // console.log(sourcePath)
-    // console.log(drawioFullPath)
     const drawioContentBuffer = await fs.readFile(sourcePath);
     const source = drawioContentBuffer.toString('utf-8');
-    
-    
-    const dataMxgraph = await parseBodyFromHtml(source);
-    const encode = he.encode(dataMxgraph);
+    let encode = '';
+    if(sourcePath.endsWith(".drawio")){
+        let wrapData = {}
+        wrapData.highlight = '#0000ff';
+        wrapData.nav = true;
+        wrapData.resize = true;
+        wrapData.toolbar = 'zoom layers tags lightbox';
+        wrapData.xml = source;
+
+        //此处一定要把\n 给转义掉，否则，可能引起页面上的js不完整，坑
+        encode = he.encode(JSON.stringify(wrapData)).replaceAll('\n',"");
+    }else{
+        const dataMxgraph = await parseBodyFromHtml(source);
+        encode = he.encode(dataMxgraph);
+    }
+
     const end = performance.now();
     console.info(`INFO\thexo-drawio:\t解析drawio文件耗时: ${context.source} -> ${drawioFilePathInPost}:\t${end - start} ms`);
 
     return `
-        <div class="mxgraph" style="max-width:100%;border:1px solid transparent;" data-mxgraph="${encode}"></div>
+        <div class="mxgraph" style="max-width:100%;border:1px solid transparent;" data-mxgraph="${encode}" ></div>
         <script type="text/javascript" src="https://viewer.diagrams.net/js/viewer-static.min.js"></script>
 `;
 }
@@ -44,6 +52,7 @@ hexo.extend.tag.register('drawio_file_path', async function(args, content) {
     if (!drawioFilePath) {
         throw new Error(`No file specified ${drawioFilePath} for tag {% plantuml_from_file %} on file ${context.source}`);
     }
+
     const drawioFullPath = path.resolve(
         path.dirname(context.full_source),
         drawioFilePath,
